0cc249cb77663f992aae8e37049d3fb1c3d4aad9
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-09-16 05:04:51 +0300

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Box, Container } from "@mui/material";
 import DeskTopNavbar from "./components/common/desktop/appbar";
 import MobileNavbar from "./components/common/mobile/NavBar";
 import MobileFooter from "./components/common/mobile/footer";
 import Login from "./components/common/Auth/Login/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import TransactionPage from "./pages/Transaction";
 import WalletPage from "./pages/Home";
 import BalancePage from "./pages/Balance";
 import HomePage from "./pages/landingPage";
 import SignUp from "./components/common/Auth/Signup/Sinup";
+import Page404 from "./pages/404";
 
+const ScrollToTop=({children})=>{
+  const location=useLocation()
+  useEffect(()=>{
+window.scrollTo(0,0)
+  },[location])
+return <>
+{children}
+</>
+}
 
 function App() {
   return (
@@ -21,8 +31,9 @@ function App() {
         <Box display={{ xs: "none", md: "block" }}>
           <DeskTopNavbar />
           <Container maxWidth="lg">
-          
+         
           <BrowserRouter>
+          <ScrollToTop>
               <Routes>
               <Route path='/' element={<WalletPage/>}/>
               <Route path='/balance' element={<BalancePage/>}/>
@@ -30,17 +41,22 @@ function App() {
                 <Route path="/landingPage" element={<HomePage/>}/>
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
+                <Route path="*" element={<Page404/>} />
               </Routes>
+              </ScrollToTop>
             </BrowserRouter>
+           
             {/* Content for desktop */}
           </Container>
         </Box>
 
         {/* Mobile Version */}
-        <Box display={{ xs: "block", md: "none", maxHeight: "100vh" }}>
+        <Box display={{ xs: "block", md: "none",  }}>
           <MobileNavbar />
           <Container maxWidth="lg">
+       
           <BrowserRouter>
+          <ScrollToTop>
               <Routes>
                 <Route path='/' element={<WalletPage/>}/>
 
@@ -48,8 +64,12 @@ function App() {
                 <Route path="/transactions" element={<TransactionPage />} />
                  <Route path="/landingPage" element={<HomePage/>}/>
                 <Route path="/login" element={<Login />} />
+                <Route path="*" element={<Page404/>} />
+                
               </Routes>
+              </ScrollToTop>
             </BrowserRouter>
+          
             </Container>
           <MobileFooter />
         </Box>
